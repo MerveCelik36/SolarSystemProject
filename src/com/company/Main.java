180e197ceef88ed4897ba6012ac98b2e386a18a8
MerveMay <mmervecelik36@gmail.com>
2022-09-01 23:43:51 -0400

    Solar system is ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,10 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Write some code here");
+        solarSystem inception= new solarSystem();
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+        System.out.println("The basic structure is ready!");
     }
 }
